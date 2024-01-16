8aa9074bcb6d550ed995ed877025ced7f7a40192
MissJP <45441369+MissJP@users.noreply.github.com>
2024-01-16 19:10:35 +0100

    Update definition_start.py
    Commit for first lesson

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -3,9 +3,15 @@
 
 
 # TODO: create a basic class
-
+class Book:
+#initializing function
+  def __init__(self, title):
+    self.title = title
 
 # TODO: create instances of the class
-
+book1 = Book("Harry Potter and Prisoner of Azkaban")
+book2 = Book("History of Art for Dummies")
 
 # TODO: print the class and property
+print(book1)
+print(book1.title)
