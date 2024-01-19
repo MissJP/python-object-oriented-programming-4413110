fc6b351c93d61e1adda664f1d95d610f0e3e115a
MissJP <45441369+MissJP@users.noreply.github.com>
2024-01-19 01:02:57 +0000

    Finished classes from chapter 4 of the course

--- Start/Ch 4/dataclass_start.py
+++ Start/Ch 4/dataclass_start.py
@@ -1,14 +1,18 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Using data classes to represent data objects
+from dataclasses import dataclass
 
-
+#we do not need to use __init__ function anymore, looks more like java
+#and automatically implement __eq__ and __repr__ magic functions
+@dataclass
 class Book:
-    def __init__(self, title, author, pages, price):
-        self.title = title
-        self.author = author
-        self.pages = pages
-        self.price = price
+    title: str
+    author: str
+    pages: int
+    price: float
 
+    def bookinfo(self):
+        return f'{self.title} with {self.pages} pages'
 
 # create some instances
 b1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
@@ -19,9 +23,12 @@ print(b1.title)
 print(b2.author)
 
 # TODO: print the book itself - dataclasses implement __repr__
-
+print(b1)
 
 # TODO: comparing two dataclasses - they implement __eq__
-
+print(b1 == b2)
 
 # TODO: change some fields
+b1.title = "Anna Karenina"
+b1.pages = 500
+print(b1.bookinfo())
--- Start/Ch 4/immutable_start.py
+++ Start/Ch 4/immutable_start.py
@@ -4,16 +4,22 @@
 from dataclasses import dataclass
 
 
-@dataclass()  # TODO: "The "frozen" parameter makes the class immutable
+@dataclass(frozen = True)  # TODO: "The "frozen" parameter makes the class immutable
 class ImmutableClass:
     value1: str = "Value 1"
     value2: int = 0
 
+    def someFunc(self, newval):
+        self.value2 = newval
 
 obj = ImmutableClass()
 print(obj.value1)
 
+#after instance initiated, data can't be changed (login, e-mail, password)
+obj2 = ImmutableClass("Another string", 20)
+print(obj2.value1, obj2.value2)
 # TODO: attempting to change the value of an immutable class throws an exception
-
-
+obj.value1 = "Another string"
+print(obj.value1)
 # TODO: even functions within the class can't change anything
+print(obj.someFunc(10))
